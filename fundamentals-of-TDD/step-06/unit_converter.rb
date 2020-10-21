d2364b15f83304d5844df8e5042900d8807a682a
Vitalii <turzhansky81@gmail.com>
2020-10-21 18:05:23 +0300

    Fundamentals of TDD: 06.Refactoring with Test Coverage

---
+++ fundamentals-of-TDD/step-06/unit_converter.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require 'rspec/autorun'
+
+DimensionalMismatchError = Class.new(StandardError)
+
+Quantity = Struct.new(:amount, :unit)
+
+class UnitConverter
+  def initialize(initial_quantity, target_unit)
+    @initial_quantity = initial_quantity
+    @target_unit = target_unit
+  end
+
+  def convert
+    Quantity.new(
+      @initial_quantity.amount * conversion_factor(from: @initial_quantity.unit, to: @target_unit),
+      @target_unit
+    )
+  end
+
+  private
+
+  CONVERSION_FACTORS = {
+    liter: {
+      cup: 4.226775,
+      liter: 1,
+      pint: 2.11338
+    },
+    gram: {
+      gram: 1,
+      kilogram: 1000
+    }
+  }.freeze
+
+  def conversion_factor(from:, to:)
+    dimension = common_dimension(from, to)
+    if !dimension.nil?
+      CONVERSION_FACTORS[dimension][to] / CONVERSION_FACTORS[dimension][from]
+    else
+      raise(DimensionalMismatchError, "can't convert from #{from} to #{to}!")
+    end
+  end
+
+  def common_dimension(from, to)
+    CONVERSION_FACTORS.keys.find do |canonical_unit|
+      CONVERSION_FACTORS[canonical_unit].keys.include?(from) &&
+        CONVERSION_FACTORS[canonical_unit].keys.include?(to)
+    end
+  end
+end
+
+describe UnitConverter do
+  describe '#convert' do
+    it 'translates between objects of the same dimension' do
+      cups = Quantity.new(2, :cup)
+      converter = UnitConverter.new(cups, :liter)
+
+      result = converter.convert
+
+      expect(result.amount).to be_within(0.001).of(0.473176)
+      expect(result.unit).to eq(:liter)
+    end
+
+    it 'can convert between quentities of the some unit' do
+      cups = Quantity.new(2, :cup)
+      converter = UnitConverter.new(cups, :cup)
+
+      result = converter.convert
+
+      expect(result.amount).to be_within(0.001).of(2)
+      expect(result.unit).to eq(:cup)
+    end
+
+    it 'raises an error if the objects are of differing dimensions' do
+      cups = Quantity.new(2, :cup)
+      converter = UnitConverter.new(cups, :grams)
+
+      expect { converter.convert }.to raise_error(DimensionalMismatchError)
+    end
+  end
+end
