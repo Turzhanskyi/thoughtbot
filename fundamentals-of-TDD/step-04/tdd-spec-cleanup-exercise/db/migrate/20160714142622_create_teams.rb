a9d45344bed84e1533d5eac32d6902bef3f3fa0a
Vitalii <turzhansky81@gmail.com>
2020-10-21 16:22:38 +0300

    Fundamentals of TDD: 04.Tdd Spec Cleanup Exercise

---
+++ fundamentals-of-TDD/step-04/tdd-spec-cleanup-exercise/db/migrate/20160714142622_create_teams.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateTeams < ActiveRecord::Migration[5.0]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.integer :owner_id
+
+      t.timestamps
+    end
+  end
+end
